08a61ed1c7e8f14f8aa4618d604943af989a14af
Raisybear <110892658+Raisybear@users.noreply.github.com>
2024-04-30 11:12:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LB426
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
